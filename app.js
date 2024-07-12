9bbc7f70d0830491645f9fa3478cb6a8f4ceea47
Beatriz <41388105+BeatrizzS@users.noreply.github.com>
2024-07-12 11:08:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -24,21 +24,20 @@ mensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(numeroSecreto);
     
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou!!!');
+        exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
-        if(chute > numeroSecreto) {
+        if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
         } else {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
-        tentativas++
+        tentativas++;
         limparCampo();
     }
 }
