e221b78efb157ade123e5fd25d583403611c91a9
Beatriz <41388105+BeatrizzS@users.noreply.github.com>
2024-07-12 11:04:01 -0300

    Update app.js
    Alterei linha 33

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ function verificarChute() {
         exibirTextoNaTela('h1', 'Acertou!!!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
       //  let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
-        exibirTextoNaTela('p', mensagemTentativas);
+       // exibirTextoNaTela('p', mensagemTentativas);
+        exibirTextoNaTela('p');
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if(chute > numeroSecreto) {
